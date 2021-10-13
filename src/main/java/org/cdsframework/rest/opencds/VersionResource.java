b300a9dac5f267978b4f4b83b6a15eaccb0f3c1d
STAN <stan@hln.com>
2021-10-13 13:02:46 -0700

    Create new VersionResource controller that exposes the version data map as injected by spring.

--- src/main/java/org/cdsframework/rest/opencds/VersionResource.java
+++ src/main/java/org/cdsframework/rest/opencds/VersionResource.java
@@ -1,5 +1,7 @@
 package org.cdsframework.rest.opencds;
 
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,8 +10,6 @@ import javax.ws.rs.core.Response;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.cdsframework.rest.opencds.pojos.HealthCheck;
-import org.opencds.config.api.VersionData;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,25 +23,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class VersionResource
 {
     private static final Log log = LogFactory.getLog(VersionResource.class);
-    private final VersionData versionData;
+    private final Map<String, String> versionData;
+
     /**
      * Creates a new instance of HealthResource
      *
      * @param versionData
      */
-    public VersionResource(VersionData versionData) {
+    public VersionResource(Map<String, String> versionData)
+    {
         this.versionData = versionData;
     }
 
     /**
      * Returns version details.
-     * 
+     *
      * @return
      * @throws JsonProcessingException
      */
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    public Response version() throws JsonProcessingException {
+    public Response version() throws JsonProcessingException
+    {
         log.debug("/version called!");
         final ObjectMapper mapper = new ObjectMapper();
         final String response = mapper.writeValueAsString(versionData);
